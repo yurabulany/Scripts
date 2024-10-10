af985b4d766e16a5dd0b4d65e1dc8a77e563b6ec
yurabulany <yura.bulany@gmail.com>
2024-10-10 13:35:06 +0200

    Added git repos, some apps + some flatpaks

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 sudo dnf update
 
 # array with the packages needed to install
-packages=(wget git neofetch htop mpv neovim vifm steam firefox thunderbird i3 picom feh dunst fish calibre transmission polybar zathura)
+packages=(wget git neofetch htop mpv neovim vifm steam firefox thunderbird i3 redshift gxkb picom feh nitrogen dunst fish calibre transmission polybar zathuraarandr j4-dmenu-desktop flameshot)
 
 # command to install the packages from the array above
 sudo dnf install -y "${packages[@]}"
@@ -26,18 +26,25 @@ flatpak install flathub org.onlyoffice.desktopeditors
 flatpak install flathub com.github.johnfactotum.Foliate
 flatpak install flathub com.viber.Viber
 flatpak install flathub io.github.shiftey.Desktop
+flatpak install flathub com.github.hluk.copyq
+
 
 # adding config files for i3 
 mkdir ~/.config/i3
 cd ~/.config/i3
-git clone 
+git clone https://github.com/yurabulany/i3-window-manager.git
 
 # adding config files for dunst 
 mkdir ~/.config/dunst
 cd ~/.config/dunst
-git clone 
+git clone https://github.com/yurabulany/Dunst-config.git
 #
 # adding config files for picom 
 mkdir ~/.config/picom
 cd ~/.config/picom
-git clone 
+git clone https://github.com/yurabulany/Picom.git
+
+# adding config files for gxkb 
+mkdir ~/.config/gxkb
+cd ~/.config/gxkb
+git clone https://github.com/yurabulany/gxkb.git
