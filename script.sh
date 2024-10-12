b3c6482b919f77917a6ab3133344aed3b7f07b60
yurabulany <yura.bulany@gmail.com>
2024-10-12 15:39:33 +0200

    updated the flatpak section + added config repos

--- script.sh
+++ script.sh
@@ -6,30 +6,28 @@
 sudo dnf update -y
 
 # array with the packages needed to install
-packages=(wget git neofetch htop mpv vim neovim vifm steam firefox thunderbird i3 i3lock redshift gxkb picom feh nitrogen dunst fish calibre transmission polybar zathura arandr j4-dmenu-desktop flameshot)
+packages=(wget git neofetch htop mpv vim neovim vifm fzf steam firefox thunderbird i3 i3lock redshift gxkb picom feh nitrogen dunst fish calibre transmission polybar zathura arandr j4-dmenu-desktop tmux flameshot darktable)
 
 # command to install the packages from the array above
 sudo dnf install -y ${packages[@]}
+
+# add fusion free and non-free repos
+sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 # installing codecs
 sudo dnf install -y gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
 sudo dnf install -y lame\* --exclude=lame-devel
 sudo dnf group -y upgrade --with-optional Multimedia
 
-# add fusion free and non-free repos
-sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-
 # add flatpak repo
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-#install flatpak apps
-flatpak install -y flathub org.telegram.desktop
-flatpak install -y flathub org.onlyoffice.desktopeditors
-flatpak install -y flathub com.github.johnfactotum.Foliate
-flatpak install -y flathub com.viber.Viber
-flatpak install -y flathub io.github.shiftey.Desktop
-flatpak install -y flathub com.github.hluk.copyq
+# array with flatpak packages
+flatpak_packages = (org.telegram.desktop org.onlyoffice.desktopeditors com.github.johnfactotum.Foliate com.viber.Viber io.github.shiftey.Desktop com.github.hluk.copyq)
+
+#install flatpak apps from the array above
+flatpak install -y ${flatpak_packages[@]}
 
 # adding config files
 cd ~/.config
@@ -37,6 +35,9 @@ git clone https://github.com/yurabulany/i3.git
 git clone https://github.com/yurabulany/gxkb.git
 git clone https://github.com/yurabulany/picom.git
 git clone https://github.com/yurabulany/dunst.git
+git clone https://github.com/yurabulany/vifm.git
+git clone https://github.com/yurabulany/fish.git
+git clone https://github.com/yurabulany/tmux.git
+git clone https://github.com/yurabulany/mpv.git
 
-
-
+reboot
